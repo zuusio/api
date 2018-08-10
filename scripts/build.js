a03c2409c9a6d7f5ef615df4ff123dbf41346cc0
Jake Skinner <jake@zarloc.com>
2018-08-10 10:06:45 -0600

    Sheesh - finally.

--- scripts/build.js
+++ scripts/build.js
@@ -13,15 +13,11 @@ exec("npm run swagger bundle --        -o web_deploy/swagger.json");
 exec("npm run swagger bundle -- --yaml -o web_deploy/swagger.yaml");
 
 var SWAGGER_UI_DIST = Path.dirname(require.resolve("swagger-ui"));
-console.log("================Just swaggering through");
-console.log(SWAGGER_UI_DIST);
 rm("-rf", "web_deploy/swagger-ui/");
 cp("-R", SWAGGER_UI_DIST, "web_deploy/swagger-ui/");
-console.log("=================aaarrrgghhh");
 sed(
   "-i",
   "http://petstore.swagger.io/v2/swagger.json",
   "../swagger.json",
   "web_deploy/swagger-ui/index.html"
 );
-console.log("========================finished petstore");
